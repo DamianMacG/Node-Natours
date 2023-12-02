60a449230d3e7a2fc6deb59b2ed993a4ddcaba68
damiang <damiangildea12@gmail.com>
2023-12-02 09:32:00 +0000

    Setting up get and post requests!

--- starter/app.js
+++ starter/app.js
@@ -2,7 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Hello World - from the server side!", app: "Natours!" });
+});
+
+app.post("/", (req, res) => {
+  res.send("I AM A POST");
+});
+
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-})
+  console.log(`App running on port ${port}...`);
+});
