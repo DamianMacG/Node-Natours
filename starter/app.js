ac022804088c4f6a1ac417578a63b1f330eb8534
damiang <damiangildea12@gmail.com>
2023-12-02 13:39:03 +0000

    Posts new tours to the data file

--- starter/app.js
+++ starter/app.js
@@ -18,23 +18,38 @@ app.use(express.json());
 
 // BELOW ----- naming it x (previously tours aswell in the GET further down) so we can see the difference between the tours. Best practice is to use tours also. If names are the same you can write a single tours with no colon
 
-const x = JSON.parse(
+const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
-    results: x.length,
+    results: tours.length,
     data: {
-      tours: x,
+      tours: tours,
     },
   });
 });
 
 app.post("/api/v1/tours", (req, res) => {
-  console.log(req.body);
-  res.send("Done");
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
