6696ae1a324c7988f13c96099a5f9f7f8d7fc2f2
damiang <damiangildea12@gmail.com>
2023-12-04 11:04:11 +0000

    File restructuring for routes and controllers

--- starter/app.js
+++ starter/app.js
@@ -2,176 +2,23 @@ const fs = require("fs");
 const express = require("express");
 const morgan = require("morgan");
 
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
 const app = express();
 
-// MIDDLEWARE
-
-// Middleware is a function that receives the request and response objects - Executes code in order so placement is important
+// MIDDLEWARE - is a function that receives the request and response objects - Executes code in order so placement is important
 app.use(morgan("dev"));
 app.use(express.json());
-
-// Custom middleware - Place this in different places in code below while testing on postman to show placement is important
-// app.use((req, res, next) => {
-//   console.log("Hello for the middleware");
-//   // Always call next() at the end
-//   next();
-// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-// ROUTE HANDLERS / CONTROLLERS
-
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  // Below:  you could also multiply req.params.id * 1 - a JS trick
-  const id = parseInt(req.params.id);
-
-  // Also below you could use - if (!tour)
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-// PUT - Generally for updating entire object
-// PATCH - Updates only specific properties
-
-const updateTour = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "----Updated Tour----",
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined yet",
-  });
-};
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined yet",
-  });
-};
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined yet",
-  });
-};
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined yet",
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined yet",
-  });
-};
-
-// app.get("/api/v1/tours", getAllTours);
-// app.get("/api/v1/tours/:id", getTour);
-// app.post("/api/v1/tours", createTour);
-// app.patch("/api/v1/tours/:id", updateTour);
-// app.delete("/api/v1/tours/:id", deleteTour);
-
-// Does the same as above commented out code
-
-//ROUTES
-
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-tourRouter.route("/").get(getAllTours).post(createTour);
-
-tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
-
-userRouter.route("/").get(getAllUsers).post(createUser);
-
-userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
-
+// ROUTES
 app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/tours", userRouter);
+app.use("/api/v1/users", userRouter);
 
 // START SERVER
 const port = 3000;
