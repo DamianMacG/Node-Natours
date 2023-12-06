48a5130977e00a924400527738d2f83f17f12737
damiang <damiangildea12@gmail.com>
2023-12-06 09:55:42 +0000

    Setting up a basic schema

--- starter/server.js
+++ starter/server.js
@@ -17,8 +17,24 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("SUCCESS!!!");
+    console.log("---------- SUCCESS!!! ----------");
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
