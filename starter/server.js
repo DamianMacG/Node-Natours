6de2e0f691c8b349e4486505c2bfc022a58b346a
damiang <damiangildea12@gmail.com>
2023-12-06 09:38:59 +0000

    COnnection to database successful

--- starter/server.js
+++ starter/server.js
@@ -1,6 +1,24 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+const mongoose = require("mongoose");
 const app = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("SUCCESS!!!");
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
