e9759c82447591f4c3a1746c34ba6ee151ff9de0
damiang <damiangildea12@gmail.com>
2023-12-13 11:20:24 +0000

    Created import file for data to DB

---
+++ starter/dev-data/data/import-dev-data.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const Tour = require("./../../models/tourModel");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("---------- SUCCESS!!! ----------");
+  });
+
+// Read JSON file
+
+const tours = JSON.parse(fs.readFileSync("tours-simple.json", "utf-8"));
+
+// Import data in DB
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("DATA SUCCESSFULLY LOADED ------------------");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Delete all data from DB
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("DATA SUCCESSFULLY ------------ DELETED");
+  } catch (err) {
+    console.log(err);
+  }
+};
