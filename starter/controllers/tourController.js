6b9534e5cb0a04845d42ef3540b28acb900a33e0
damiang <damiangildea12@gmail.com>
2023-12-13 09:09:07 +0000

    Successfully create a tour to the database

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -1,18 +1,5 @@
 const Tour = require("./../models/tourModel");
 
-exports.checkBody = (req, res, next) => {
-  console.log(req.body.name);
-  console.log(req.body.price);
-
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Missing name or price",
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
@@ -34,13 +21,22 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: "success",
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
