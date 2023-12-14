48063aab3d29aa374556bc45159cdc210c387a26
damiang <damiangildea12@gmail.com>
2023-12-14 09:14:25 +0000

    CHanging await of query for future pagination etc

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -2,20 +2,26 @@ const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
+
+    // BUILD QUERY
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
 
-    const tours = await Tour.find(queryObj);
+    const query = await Tour.find(queryObj);
 
     // Mongoose filtering method v
 
-    // const tours = await Tour.find()
+    // const query = await Tour.find()
     //   .where("duration")
     //   .equals(5)
     //   .where("difficulty")
     //   .equals("easy");
+
+
+    // EXECUTE QUERY
+    const tours = await query;
 
     res.status(200).json({
       status: "success",
