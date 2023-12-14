689cccea87cae9254720aab4e9b76ee4562ee016
damiang <damiangildea12@gmail.com>
2023-12-14 10:54:15 +0000

    Successfully added pagination feature

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -20,17 +20,29 @@ exports.getAllTours = async (req, res) => {
       const sortBy = req.query.sort.split(",").join(" ");
       query = query.sort(sortBy);
     } else {
-      query = query.sort("-createdAt");
+      query = query.sort("-createdAt _id");
     }
 
     // 3) Field limiting
     if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ')
-      query = query.select(fields)
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
     } else {
-      query = query.select('-__v')
+      query = query.select("-__v");
     }
 
+    // Pagination
+    // Convert string to a number by * 1, then default value
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=3&limit=10, 1-10 page 1, 11-20 page 2, etc...
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist!");
+    }
 
     // EXECUTE QUERY
     const tours = await query;
