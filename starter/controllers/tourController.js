466cc721c51830c24e9c7597206f659e0987241f
damiang <damiangildea12@gmail.com>
2023-12-14 09:07:57 +0000

    Successfully added excluded fields in filtering

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -2,17 +2,20 @@ const Tour = require("./../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
-    // const tours = await Tour.find({
-    //   duration: 5,
-    //   difficulty: "easy",
-    // });
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-    const tours = await Tour.find()
-      .where("duration")
-      .equals(5)
-      .where("difficulty")
-      .equals("easy");
+
+    const tours = await Tour.find(queryObj);
+
+    // Mongoose filtering method v
+
+    // const tours = await Tour.find()
+    //   .where("duration")
+    //   .equals(5)
+    //   .where("difficulty")
+    //   .equals("easy");
 
     res.status(200).json({
       status: "success",
