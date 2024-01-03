2ee225896cd2392888c101ad85591427e397f70c
damiang <damiangildea12@gmail.com>
2024-01-03 09:50:39 +0000

    Added simple error handling for routes that don't exist

--- starter/app.js
+++ starter/app.js
@@ -5,8 +5,7 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// MIDDLEWARE - is a function that receives the request and response objects - Executes code in order so placement is important
-
+// MIDDLEWARE - is a function that receives the request and response objects - Executes code in order, so placement is important
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -16,5 +15,12 @@ app.use(express.static(`${__dirname}/public`));
 // ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  })
+})
 
 module.exports = app;
