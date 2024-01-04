51cb95e114a9f28e55e6547aacab64bbacd62375
damiang <damiangildea12@gmail.com>
2024-01-04 17:17:30 +0000

    Added to all controllers that use an ID

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -65,6 +65,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+
+  if (!tour) {
+    return next(new AppError("No tour with that ID", 404));
+  }
+
   res.status(200).json({
     status: "success",
     data: {
@@ -75,6 +80,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError("No tour with that ID", 404));
+  }
+
   res.status(204).json({
     status: "success",
     data: null,
