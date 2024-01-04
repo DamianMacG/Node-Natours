2b3797d8d448e1173871d3804f52864370fe8a53
damiang <damiangildea12@gmail.com>
2024-01-04 17:13:26 +0000

    testing App Error in get tour for invalid ID

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -1,6 +1,8 @@
 const Tour = require("./../models/tourModel");
 const APIFeatures = require("./../utils/apiFeatures");
 const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
   req.query.sort = "-ratingsAverage,price";
@@ -29,6 +31,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError("No tour with that ID", 404));
+  }
   res.status(200).json({
     status: "success",
     data: {
