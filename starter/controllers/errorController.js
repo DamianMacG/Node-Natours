6d0b9c4617e37e7bdc23e1a9137077d020ae9928
damiang <damiangildea12@gmail.com>
2024-01-06 14:39:11 +0000

    Added validation error handling in errorController

--- starter/controllers/errorController.js
+++ starter/controllers/errorController.js
@@ -10,6 +10,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400)
 };
 
+const handleValidationErrorDB = (err) => {
+const errors = Object.values(err.errors).map((el) => el.message)
+  const message = `Invalid input data. ${errors.join('. ')}`
+  return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -48,6 +54,7 @@ module.exports = (err, req, res, next) => {
     // Must remain err.name as above doesn't transfer name key
     if (err.name === "CastError") error = handleCastErrorDB(error);
     if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if(err.name === 'ValidationError') error = handleValidationErrorDB(error)
     sendErrorProd(error, res);
   }
 };
