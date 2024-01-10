04af5dc5f260ee9d076fabff8b28bedd5cfa5ff8
damiang <damiangildea12@gmail.com>
2024-01-10 10:56:45 +0000

    Fixed sign up security error

--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -1,13 +1,18 @@
-const User = require('./../models/userModel')
+const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.signUp = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body)
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-    res.status(201).json({
-        status: 'success',
-        data: {
-            user: newUser
-        }
-    })
-})
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: newUser,
+    },
+  });
+});
