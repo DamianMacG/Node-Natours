2e81c2a549f2dfe1164f610438ed696a03aecea9
damiang <damiangildea12@gmail.com>
2024-01-10 11:33:04 +0000

    Adding login

--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const jwt = require("jsonwebtoken");
+const AppError = require("./../utils/appError");
 
 exports.signUp = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -22,3 +23,17 @@ exports.signUp = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+   return next(new AppError("Please provide email and password", 400));
+  }
+
+  const token = "";
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+};
