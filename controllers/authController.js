03d5d87fb16ef627f6d4267bf2d16077d04e0f14
damiang <damiangildea12@gmail.com>
2024-01-12 10:39:34 +0000

    Testing if user still exists error response with token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const { promisify } = require('util');
+const { promisify } = require("util");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const jwt = require("jsonwebtoken");
@@ -55,23 +55,33 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
-// 1) Get token and check it's there
-let token;
-if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-  token = req.headers.authorization.split(' ')[1]
-}
+  // 1) Get token and check it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 
-if(!token) {
-  return next(new AppError('You are not logged in. Please log in to get access.', 401))
-}
-// 2) Verification token
-const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
-console.log(decoded)
+  if (!token) {
+    return next(
+      new AppError("You are not logged in. Please log in to get access.", 401)
+    );
+  }
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  console.log(decoded);
 
-// 3) Check if user still exists
+  // 3) Check if user still exists
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(
+      new AppError("The User belonging to this token no longer exists", 401)
+    );
+  }
 
-// 4) Check if user changed password after the JWT issued
+  // 4) Check if user changed password after the JWT issued
 
-
-  next()
-})
+  next();
+});
