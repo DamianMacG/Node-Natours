8474a7fd2574a77419ccd50d173f9f5f47408c16
damiang <damiangildea12@gmail.com>
2024-01-12 09:58:05 +0000

    testing JWT error in production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,15 +6,18 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value.`
-  return new AppError(message, 400)
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value.`;
+  return new AppError(message, 400);
 };
 
 const handleValidationErrorDB = (err) => {
-const errors = Object.values(err.errors).map((el) => el.message)
-  const message = `Invalid input data. ${errors.join('. ')}`
-  return new AppError(message, 400)
-}
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+const handleJWTError = (err) =>
+  new AppError("Invalid token, please login again", 401);
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -54,7 +57,9 @@ module.exports = (err, req, res, next) => {
     // Must remain err.name as above doesn't transfer name key
     if (err.name === "CastError") error = handleCastErrorDB(error);
     if (err.code === 11000) error = handleDuplicateFieldsDB(error);
-    if(err.name === 'ValidationError') error = handleValidationErrorDB(error)
+    if (err.name === "ValidationError") error = handleValidationErrorDB(error);
+    if (err.name === "JsonWebTokenError") error = handleJWTError(error);
+
     sendErrorProd(error, res);
   }
 };
