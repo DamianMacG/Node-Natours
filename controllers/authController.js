35deb0b5ffbb345f04a741abaee776b2b17e5d79
damiang <damiangildea12@gmail.com>
2024-01-17 18:12:09 +0000

    Working on restrictTo function error

--- controllers/authController.js
+++ controllers/authController.js
@@ -95,3 +95,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo() = (...roles) => {
+  return (req, res, next) => {
+    // roles is an array i.e ['admin', 'lead-guide']. role = 'user (default)
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+    next();
+  };
+};
