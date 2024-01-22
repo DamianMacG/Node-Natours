70d35bd887405ffdfce4d87ae8d4376b909ef7c4
damiang <damiangildea12@gmail.com>
2024-01-22 19:49:06 +0000

    Implemented sanitization for our NoSQL queries

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
@@ -25,14 +27,18 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, try again in an hour.",
 });
-
-
 app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
 
-// Server static files
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
