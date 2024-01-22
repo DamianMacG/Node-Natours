c88ff195458cacca7d204162e74b8573357f152d
damiang <damiangildea12@gmail.com>
2024-01-22 09:11:49 +0000

    Adding sendEmail functionality

--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = (options) => {
+const sendEmail = async (options) => {
   // 1. Create a transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
@@ -13,16 +13,16 @@ const sendEmail = (options) => {
   });
 
   // 2. Define the email options
-const mailOptions = {
-    from: 'Damian <damian@damian.com>',
+  const mailOptions = {
+    from: "Damian <damian@damian.com>",
     to: options.email,
     subject: options.subject,
     text: options.message,
     // html:
-}
-
+  };
 
   // 3. Actually send the email
+  await transporter.sendEmail(mailOptions);
 };
 
 module.exports = sendEmail;
