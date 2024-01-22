16696dad6b86e592e4b2185842155cd7d6775876
damiang <damiangildea12@gmail.com>
2024-01-22 19:22:46 +0000

    Added helmet middleware for header security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
@@ -9,21 +10,32 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // (GLOBAL) MIDDLEWARE - is a function that receives the request and response objects - Executes code in order, so placement is important
+
+// Security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, try again in an hour.",
 });
 
-app.use("/api", limiter); 
 
-app.use(express.json());
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Server static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers)
