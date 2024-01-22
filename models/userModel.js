e6a895e7bfaedde634790cb3cf01b8a1e5b203e2
damiang <damiangildea12@gmail.com>
2024-01-22 10:36:31 +0000

    Added passwordChangedAt middleware, resetpassword functionality working also

--- models/userModel.js
+++ models/userModel.js
@@ -56,6 +56,14 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
+
+  // - 1000 to make sure token has always been created after
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 userSchema.methods.correctPassword = async function (
   candidatePassword,
   userPassword
@@ -84,7 +92,7 @@ userSchema.methods.createPasswordResetToken = function () {
     .update(resetToken)
     .digest("hex");
 
-    console.log({resetToken}, this.passwordResetToken)
+  console.log({ resetToken }, this.passwordResetToken);
   // 10 minutes
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
