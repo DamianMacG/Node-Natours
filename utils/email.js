427edfb09158828bc04a114aebdf78cda192db59
damiang <damiangildea12@gmail.com>
2024-01-22 09:00:56 +0000

    Updating env variables and expnading sendEmail function

--- utils/email.js
+++ utils/email.js
@@ -1,12 +1,28 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 
-const sendEmail = options => {
-    // 1. Create a transporter
+const sendEmail = (options) => {
+  // 1. Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      password: process.env.EMAIL_PASSWORD,
+    },
+    // Activate in gmail 'less secure app' option
+  });
 
-
-    // 2. Define the email options
-
-
-    // 3. Actually send the email
-    
+  // 2. Define the email options
+const mailOptions = {
+    from: 'Damian <damian@damian.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
 }
+
+
+  // 3. Actually send the email
+};
+
+module.exports = sendEmail;
