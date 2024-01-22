37b122f2a2bc201e2e7f19285ebc1a8771e41ff4
damiang <damiangildea12@gmail.com>
2024-01-22 19:14:23 +0000

    Implemented a rate limit of requests through all routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
@@ -7,10 +8,19 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// MIDDLEWARE - is a function that receives the request and response objects - Executes code in order, so placement is important
+// (GLOBAL) MIDDLEWARE - is a function that receives the request and response objects - Executes code in order, so placement is important
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, try again in an hour.",
+});
+
+app.use("/api", limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
