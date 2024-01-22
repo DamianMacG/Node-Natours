e47f8261761baeee63b31232a49e960c5c10ed1d
damiang <damiangildea12@gmail.com>
2024-01-22 09:57:44 +0000

    Fixed nodemailer problem

--- utils/email.js
+++ utils/email.js
@@ -1,28 +1,27 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  // 1. Create a transporter
+  // 1) Create a transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
-      password: process.env.EMAIL_PASSWORD,
+      pass: process.env.EMAIL_PASSWORD,
     },
-    // Activate in gmail 'less secure app' option
   });
 
-  // 2. Define the email options
+  // 2) Define the email options
   const mailOptions = {
-    from: "Damian <damian@damian.com>",
+    from: "Damian <damiangildea1@gmail.com>",
     to: options.email,
     subject: options.subject,
     text: options.message,
     // html:
   };
 
-  // 3. Actually send the email
-  await transporter.sendEmail(mailOptions);
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
