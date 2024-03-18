eab6e724e4971a03d925c36734a22b864d45fd8d
damiang <damiangildea12@gmail.com>
2024-03-18 19:47:44 +0000

    Added merge params to the review route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const tourController = require("./../controllers/tourController");
 const authController = require("./../controllers/authController");
-const reviewController = require("./../controllers/reviewController");
+// const reviewController = require("./../controllers/reviewController");
+const reviewRouter = require('./../routes/reviewRoutes')
 
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter)
 
 router
   .route("/top-5-cheap")
@@ -28,12 +31,12 @@ router
     tourController.deleteTour
   );
 
-router
-  .route("/:tourId/reviews")
-  .post(
-    authController.protect,
-    authController.restrictTo("user"),
-    reviewController.createReview
-  );
+// router
+//   .route("/:tourId/reviews")
+//   .post(
+//     authController.protect,
+//     authController.restrictTo("user"),
+//     reviewController.createReview
+//   );
 
 module.exports = router;
