111a7bcf5cde1e2d1c1dc4d9debaa3c7f5298670
damiang <damiangildea12@gmail.com>
2024-03-19 08:58:29 +0000

    Fix AppError constructor issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
+const factory = require("./handlerFactory");
 
 const filteredObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -76,9 +77,4 @@ exports.updateUser = (req, res) => {
     message: "This route is not defined yet",
   });
 };
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined yet",
-  });
-};
+exports.deleteUser = factory.deleteOne(User);
