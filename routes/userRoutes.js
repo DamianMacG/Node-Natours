3e892e7f896940a34813ac7e35381bc0a72daa43
damiang <damiangildea12@gmail.com>
2024-03-25 09:52:43 +0000

    Refactoring authorisation for reviews

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,23 +6,20 @@ const router = express.Router();
 
 router.post("/signup", authController.signUp);
 router.post("/login", authController.login);
-
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
-router.patch(
-  "/updateMyPassword",
-  authController.protect,
-  authController.updatePassword
-);
 
-router.get(
-  "/me",
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch("/updateMe", authController.protect, userController.updateMe);
-router.delete("/deleteMe", authController.protect, userController.deleteMe);
+// Will protect all routes after this Middleware
+router.use(authController.protect);
+
+router.patch("/updateMyPassword", authController.updatePassword);
+
+router.get("/me", userController.getMe, userController.getUser);
+router.patch("/updateMe", userController.updateMe);
+router.delete("/deleteMe", userController.deleteMe);
+
+// Restricted to only admin after this Middleware
+router.use(authController.restrictTo("admin"));
 
 router
   .route("/")
