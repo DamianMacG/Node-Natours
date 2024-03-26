9d2c72891543ba186ef0786c3b94fd8ef618da3c
damiang <damiangildea12@gmail.com>
2024-03-26 13:07:39 +0000

    Added distance calculation

--- models/tourModel.js
+++ models/tourModel.js
@@ -182,13 +182,13 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre("aggregate", function (next) {
-  this.pipeline().unshift({
-    $match: { secretTour: { $ne: true } },
-  });
-  // console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre("aggregate", function (next) {
+//   this.pipeline().unshift({
+//     $match: { secretTour: { $ne: true } },
+//   });
+
+//   next();
+// });
 
 // Capital first letter for models
 const Tour = mongoose.model("Tour", tourSchema);
