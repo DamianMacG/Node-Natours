c57b8f7c407718431eb4b81553bd548704b70751
damiang <damiangildea12@gmail.com>
2024-03-28 10:49:58 +0000

    More pug files added and app routes updated

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,15 +4,19 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.get("/", authController.isLoggedIn, viewsController.getOverview);
-router.get("/tour/:slug", authController.isLoggedIn, viewsController.getTour);
-router.get("/login", authController.isLoggedIn, viewsController.getLoginForm);
-router.get("/me", authController.protect, viewsController.getAccount);
+router.get("/", viewsController.getOverview);
 
-router.post(
-  "/submit-user-data",
-  authController.protect,
-  viewsController.updateUserData
-);
+router.get("/tour", viewsController.getTour);
+
+// router.get("/", authController.isLoggedIn, viewsController.getOverview);
+// router.get("/tour/:slug", authController.isLoggedIn, viewsController.getTour);
+// router.get("/login", authController.isLoggedIn, viewsController.getLoginForm);
+// router.get("/me", authController.protect, viewsController.getAccount);
+
+// router.post(
+//   "/submit-user-data",
+//   authController.protect,
+//   viewsController.updateUserData
+// );
 
 module.exports = router;
